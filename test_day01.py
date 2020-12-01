014df72edd73de7275e51d1eab56ac6fd279185f
Seth Guberman <10121976@bd.com>
2020-12-01 16:19:11 -0500

    refactor day01 tests

--- test_day01.py
+++ test_day01.py
@@ -1,27 +1,29 @@
 from day01 import find_n_numbers_totaling, part1, part2
 
 
+PUZZLE_INPUT = 'input_day01.txt'
+TEST_INPUT = 'test_input_day01.txt'
+EXAMPLE = [1721, 979, 366, 299, 675, 1456]
+
 def test_part1():
-    assert part1('input_day01.txt') == 1016131
+    assert part1(PUZZLE_INPUT) == 1016131
 
 
 def test_part2():
-    assert part2('input_day01.txt') == 276432018
+    assert part2(PUZZLE_INPUT) == 276432018
 
 
 def test_part1_example():
-    assert part1('test_input_day01.txt') == 514579
+    assert part1(TEST_INPUT) == 514579
 
 
 def test_part2_example():
-    assert part2('test_input_day01.txt') == 241861950
+    assert part2(TEST_INPUT) == 241861950
 
 
 def test_find_2_numbers_totaling():
-    example_list = [1721, 979, 366, 299, 675, 1456]
-    assert find_n_numbers_totaling(2, 2020, example_list) == (1721, 299)
+    assert find_n_numbers_totaling(2, 2020, EXAMPLE) == (1721, 299)
 
 
 def test_find_3_numbers_totaling():
-    example_list = [1721, 979, 366, 299, 675, 1456]
-    assert find_n_numbers_totaling(3, 2020, example_list) == (979, 366, 675)
+    assert find_n_numbers_totaling(3, 2020, EXAMPLE) == (979, 366, 675)
