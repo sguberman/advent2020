3bc4ac32e1c09224b453944bf979b145b5cc5333
Seth Guberman <10121976@bd.com>
2020-12-01 14:58:18 -0500

    add typing to day01

--- day01.py
+++ day01.py
@@ -1,24 +1,27 @@
 from itertools import combinations
+from typing import Iterable, Tuple
 
 
-def find_n_numbers_totaling(n, total, numbers):
+def find_n_numbers_totaling(n: int,
+                            total: int,
+                            numbers: Iterable[int]) -> Tuple[int, ...]:
     return next(group for group in combinations(numbers, n)
                 if sum(group) == total)
 
 
-def part1(inputfile):
-    entries = (int(entry) for entry in open(inputfile))
+def part1(filename: str) -> int:
+    entries = (int(entry) for entry in open(filename))
     first, second = find_n_numbers_totaling(2, 2020, entries)
     return first * second
 
 
-def part2(inputfile):
-    entries = (int(entry) for entry in open(inputfile))
+def part2(filename: str) -> int:
+    entries = (int(entry) for entry in open(filename))
     first, second, third = find_n_numbers_totaling(3, 2020, entries)
     return first * second * third
 
 
 if __name__ == '__main__':
-    inputfile = 'input_day01.txt'
-    print(part1(inputfile))  # 1016131
-    print(part2(inputfile))  # 276432018
+    puzzle_input = 'input_day01.txt'
+    print(part1(puzzle_input))  # 1016131
+    print(part2(puzzle_input))  # 276432018
