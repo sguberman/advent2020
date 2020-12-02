a5c56e5cfe21d9308ad68dbe97c488b629205c00
Seth Guberman <sguberman@gmail.com>
2020-12-02 15:58:19 -0500

    parametrize day02 tests

--- test_day02.py
+++ test_day02.py
@@ -1,32 +1,49 @@
+import pytest
+
 from day02 import is_valid_positions, is_valid_times, parse_line, part1, part2
 
 PUZZLE_INPUT = 'input_day02.txt'
 TEST_INPUT = 'test_input_day02.txt'
 
 
-def test_parse_line():
-    assert parse_line('1-3 a: abcde') == ((1, 3), 'a', 'abcde')
-    assert parse_line('1-3 b: cdefg') == ((1, 3), 'b', 'cdefg')
-    assert parse_line('2-9 c: ccccccccc') == ((2, 9), 'c', 'ccccccccc')
+@pytest.mark.parametrize('line, expected', [
+    ('1-3 a: abcde', ((1, 3), 'a', 'abcde')),
+    ('1-3 b: cdefg', ((1, 3), 'b', 'cdefg')),
+    ('2-9 c: ccccccccc', ((2, 9), 'c', 'ccccccccc')),
+])
+def test_parse_line(line, expected):
+    assert parse_line(line) == expected
 
 
-def test_is_valid_times():
-    assert is_valid_times((1, 3), 'a', 'abcde') == True
-    assert is_valid_times((1, 3), 'b', 'cdefg') == False
-    assert is_valid_times((2, 9), 'c', 'ccccccccc') == True
+@pytest.mark.parametrize('times, letter, password, expected', [
+    ((1, 3), 'a', 'abcde', True),
+    ((1, 3), 'b', 'cdefg', False),
+    ((2, 9), 'c', 'ccccccccc', True),
+])
+def test_is_valid_times(times, letter, password, expected):
+    assert is_valid_times(times, letter, password) == expected
 
 
-def test_is_valid_positions():
-    assert is_valid_positions((1, 3), 'a', 'abcde') == True
-    assert is_valid_positions((1, 3), 'b', 'cdefg') == False
-    assert is_valid_positions((2, 9), 'c', 'ccccccccc') == False
+@pytest.mark.parametrize('positions, letter, password, expected', [
+    ((1, 3), 'a', 'abcde', True),
+    ((1, 3), 'b', 'cdefg', False),
+    ((2, 9), 'c', 'ccccccccc', False),
+])
+def test_is_valid_positions(positions, letter, password, expected):
+    assert is_valid_positions(positions, letter, password) == expected
 
 
-def test_part1():
-    assert part1(TEST_INPUT) == 2
-    assert part1(PUZZLE_INPUT) == 454
+@pytest.mark.parametrize('puzzle_input, answer', [
+    (TEST_INPUT, 2),
+    (PUZZLE_INPUT, 454),
+])
+def test_part1(puzzle_input, answer):
+    assert part1(puzzle_input) == answer
 
 
-def test_part2():
-    assert part2(TEST_INPUT) == 1
-    assert part2(PUZZLE_INPUT) == 649
+@pytest.mark.parametrize('puzzle_input, answer', [
+    (TEST_INPUT, 1),
+    (PUZZLE_INPUT, 649),
+])
+def test_part2(puzzle_input, answer):
+    assert part2(puzzle_input) == answer
