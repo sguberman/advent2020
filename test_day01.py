88fd17fe0d5a9336344a46002f9a1ff1f7e683d0
Seth Guberman <sguberman@gmail.com>
2020-12-02 16:08:57 -0500

    parametrize day01 tests

--- test_day01.py
+++ test_day01.py
@@ -1,29 +1,31 @@
-from day01 import find_n_numbers_totaling, part1, part2
+import pytest
 
+from day01 import find_n_numbers_totaling, part1, part2
 
 PUZZLE_INPUT = 'input_day01.txt'
 TEST_INPUT = 'test_input_day01.txt'
 EXAMPLE = [1721, 979, 366, 299, 675, 1456]
 
-def test_part1():
-    assert part1(PUZZLE_INPUT) == 1016131
+
+@pytest.mark.parametrize('puzzle_input, expected', [
+    (TEST_INPUT, 514579),
+    (PUZZLE_INPUT, 1016131),
+])
+def test_part1(puzzle_input, expected):
+    assert part1(puzzle_input) == expected
 
 
-def test_part2():
-    assert part2(PUZZLE_INPUT) == 276432018
+@pytest.mark.parametrize('puzzle_input, expected', [
+    (TEST_INPUT, 241861950),
+    (PUZZLE_INPUT, 276432018),
+])
+def test_part2(puzzle_input, expected):
+    assert part2(puzzle_input) == expected
 
 
-def test_part1_example():
-    assert part1(TEST_INPUT) == 514579
-
-
-def test_part2_example():
-    assert part2(TEST_INPUT) == 241861950
-
-
-def test_find_2_numbers_totaling():
-    assert find_n_numbers_totaling(2, 2020, EXAMPLE) == (1721, 299)
-
-
-def test_find_3_numbers_totaling():
-    assert find_n_numbers_totaling(3, 2020, EXAMPLE) == (979, 366, 675)
+@pytest.mark.parametrize('n, total, numbers, expected', [
+    (2, 2020, EXAMPLE, (1721, 299)),
+    (3, 2020, EXAMPLE, (979, 366, 675)),
+])
+def test_find_n_numbers_totaling(n, total, numbers, expected):
+    assert find_n_numbers_totaling(n, total, numbers) == expected
