c742f9eca09a76b9bdbeecbaf514818cf39739c0
Seth Guberman <sguberman@gmail.com>
2020-12-03 00:17:38 -0500

    solve day03 part2

--- day03.py
+++ day03.py
@@ -1,9 +1,8 @@
-def part1(puzzle_input):
+def part1(puzzle_input, dx, dy):
     tree_map = [line.strip() for line in open(puzzle_input)]
     width = len(tree_map[0])
     height = len(tree_map)
     x, y = 0, 0
-    dx, dy = 3, 1
     tree_count = 0
     while y < height:
         x = (x + dx) % width
@@ -15,6 +14,15 @@ def part1(puzzle_input):
     return tree_count
 
 
+def part2(puzzle_input):
+    result = 1
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    for dx, dy in slopes:
+        result *= part1(puzzle_input, dx, dy)
+    return result
+
+
 if __name__ == '__main__':
     puzzle_input = 'input_day03.txt'
-    print(part1(puzzle_input))
+    print(part1(puzzle_input, 3, 1))
+    print(part2(puzzle_input))
