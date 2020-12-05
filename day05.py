f7c7dc7a92585a2de78ea446e23366c568694efd
Seth Guberman <sguberman@gmail.com>
2020-12-05 00:03:32 -0600

    add typing to day05

--- day05.py
+++ day05.py
@@ -1,12 +1,15 @@
-def boarding_passes(filename):
+from typing import Iterator
+
+
+def boarding_passes(filename: str) -> Iterator[str]:
     return (line.strip() for line in open(filename))
 
 
-def seat_id(boarding_pass):
+def seat_id(boarding_pass: str) -> int:
     return row(boarding_pass) * 8 + column(boarding_pass)
 
 
-def partition(minn, maxx, halves):
+def partition(minn: int, maxx: int, halves: str) -> int:
     for half in halves:
         mid = minn + (maxx - minn) // 2
         if half in 'FL':
@@ -16,23 +19,23 @@ def partition(minn, maxx, halves):
     return minn
 
 
-def row(boarding_pass):
+def row(boarding_pass: str) -> int:
     halves = boarding_pass[:7]
     minn, maxx = 0, 127
     return partition(minn, maxx, halves)
 
 
-def column(boarding_pass):
+def column(boarding_pass: str) -> int:
     halves = boarding_pass[-3:]
     minn, maxx = 0, 7
     return partition(minn, maxx, halves)
 
 
-def part1(filename):
+def part1(filename: str) -> int:
     return max(seat_id(bp) for bp in boarding_passes(filename))
 
 
-def part2(filename):
+def part2(filename: str) -> int:
     ids = [seat_id(bp) for bp in boarding_passes(filename)]
     return next(id for id in range(min(ids), max(ids) + 1) if id not in ids)
 
