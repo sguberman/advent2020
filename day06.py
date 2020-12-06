cbc85263c8cc7d0fee99bd4f01b7ddece666afc2
Seth Guberman <sguberman@gmail.com>
2020-12-06 13:33:15 -0600

    add typing to day06

--- day06.py
+++ day06.py
@@ -1,36 +1,35 @@
-def read_groups(filename):
+from typing import Iterator, List
+
+
+def groups(filename: str) -> Iterator[List[str]]:
     with open(filename) as f:
-        group = []
+        group: List[str] = []
         for line in f:
             if line == '\n':
                 yield group
                 group = []
             else:
                 group.append(line.strip())
-        yield group
+        yield group  # last group at end of file has no extra newline
 
 
-def anyone_answers(group):
-    answers = set()
-    for individual_answers in group:
-        answers.update(individual_answers)
-    return answers
+def anyone_answers(group: List[str]) -> set:
+    return set.union(*[set(x) for x in group])
 
 
-def everyone_answers(group):
-    individual_answers = [set(x) for x in group]
-    return individual_answers[0].intersection(*individual_answers[1:])
+def everyone_answers(group: List[str]) -> set:
+    return set.intersection(*[set(x) for x in group])
 
 
 def part1(filename):
-    return sum(len(anyone_answers(group)) for group in read_groups(filename))
+    return sum(len(anyone_answers(group)) for group in groups(filename))
 
 
 def part2(filename):
-    return sum(len(everyone_answers(group)) for group in read_groups(filename))
+    return sum(len(everyone_answers(group)) for group in groups(filename))
 
 
 if __name__ == '__main__':
     puzzle_input = 'input_day06.txt'
-    print(part1(puzzle_input))
-    print(part2(puzzle_input))
+    print(part1(puzzle_input))  # 6630
+    print(part2(puzzle_input))  # 3437
