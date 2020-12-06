da2c71e94b5e6ea6ab1d120063712d6a4d88b392
Seth Guberman <sguberman@gmail.com>
2020-12-06 13:20:24 -0600

    solve day06 part 2

--- day06.py
+++ day06.py
@@ -10,17 +10,27 @@ def read_groups(filename):
         yield group
 
 
-def group_answers(group):
+def anyone_answers(group):
     answers = set()
     for individual_answers in group:
         answers.update(individual_answers)
     return answers
 
 
+def everyone_answers(group):
+    individual_answers = [set(x) for x in group]
+    return individual_answers[0].intersection(*individual_answers[1:])
+
+
 def part1(filename):
-    return sum(len(group_answers(group)) for group in read_groups(filename))
+    return sum(len(anyone_answers(group)) for group in read_groups(filename))
+
+
+def part2(filename):
+    return sum(len(everyone_answers(group)) for group in read_groups(filename))
 
 
 if __name__ == '__main__':
     puzzle_input = 'input_day06.txt'
     print(part1(puzzle_input))
+    print(part2(puzzle_input))
