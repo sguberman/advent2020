1402f715e0203af3a4b62979ab613c12b3ae94e3
Seth Guberman <sguberman@gmail.com>
2020-12-07 16:56:08 -0500

    change argument order in day07

--- day07.py
+++ day07.py
@@ -24,8 +24,8 @@ def read_rules(filename: str) -> RuleDict:
     return rules
 
 
-def does_contain(outer_bag: str,
-                 target_bag: str,
+def does_contain(target_bag: str,
+                 outer_bag: str,
                  filename: str,
                  rules: RuleDict,
                  cache: ContainsCache = {}) -> bool:
@@ -38,27 +38,27 @@ def does_contain(outer_bag: str,
             return True
         else:
             for inner_bag in inner_bags:
-                if does_contain(inner_bag, target_bag, filename, rules, cache):
+                if does_contain(target_bag, inner_bag, filename, rules, cache):
                     cache[(filename, outer_bag, target_bag)] = True
                     return True
             cache[(filename, outer_bag, target_bag)] = False
             return False
 
 
-def number_contained(rules: RuleDict, bag: str) -> int:
+def number_contained(bag: str, rules: RuleDict) -> int:
     children = rules[bag]
     return (len(children) +
-            sum(number_contained(rules, child) for child in children))
+            sum(number_contained(child, rules) for child in children))
 
 
 def part1(filename: str) -> int:
     rules = read_rules(filename)
-    return sum(does_contain(bag, 'shiny gold', filename, rules) for bag in rules)
+    return sum(does_contain('shiny gold', bag, filename, rules) for bag in rules)
 
 
 def part2(filename: str) -> int:
     rules = read_rules(filename)
-    return number_contained(rules, 'shiny gold')
+    return number_contained('shiny gold', rules)
 
 
 if __name__ == '__main__':
