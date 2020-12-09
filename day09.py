899eea7712f8ebde30ee7676afa123b6b8bbac22
Seth Guberman <sguberman@gmail.com>
2020-12-09 15:58:10 -0500

    solve day09 part 1

--- day09.py
+++ day09.py
@@ -1,8 +1,53 @@
+from collections import defaultdict
+from itertools import combinations, islice
+from typing import Dict, Iterable, Optional
+
 from utils import elapsed_time, print_results
 
 
-def part1(filename: str) -> int:
-    pass
+def stream_numbers(filename: str) -> Iterable[int]:
+    return (int(line) for line in open(filename))
+
+
+def all_sums(preamble: Iterable[int]) -> Dict[int, set]:
+    sums = defaultdict(set)
+    for first, second in combinations(preamble, 2):
+        sums[first].add(first + second)
+    sums[second] = set()
+    return sums
+
+
+def update_sums(sums: Dict[int, set], new_number: int) -> Dict[int, set]:
+    del(sums[list(sums.keys())[0]])  # remove the first (oldest) key
+    for key in sums:
+        sums[key].add(key + new_number)
+    sums[new_number] = set()
+    return sums
+
+
+def is_valid2(number: int, sums: Dict[int, set]) -> bool:
+    return any(number in value for value in sums.values())
+
+
+def is_valid(number: int, preamble: Iterable[int]) -> bool:
+    return number in (sum(combo) for combo in combinations(preamble, 2))
+
+
+def first_invalid(numbers: Iterable[int],
+                  preamble_len: int = 25) -> Optional[int]:
+    preamble = islice(numbers, preamble_len)
+    sums = all_sums(preamble)
+    for new_number in numbers:
+        if not is_valid2(new_number, sums):
+            return new_number
+        else:
+            sums = update_sums(sums, new_number)
+    return None
+
+
+def part1(filename: str, preamble_len: int = 25) -> Optional[int]:
+    numbers = stream_numbers(filename)
+    return first_invalid(numbers, preamble_len)
 
 
 def part2(filename: str) -> int:
@@ -10,6 +55,6 @@ def part2(filename: str) -> int:
 
 
 if __name__ == '__main__':
-    puzzle_input = 'test_input_day09.txt'
+    puzzle_input = 'input_day09.txt'
     print_results(elapsed_time(part1, puzzle_input),
                   elapsed_time(part2, puzzle_input))
--- test_day09.py
+++ test_day09.py
@@ -1,10 +1,33 @@
 import pytest
 
-from day09 import part1, part2
+from day09 import (all_sums, first_invalid, is_valid, is_valid2, part1, part2,
+                   update_sums)
 
 PUZZLE_INPUT = 'input_day09.txt'
 TEST_INPUT = 'test_input_day09.txt'
-EXAMPLE = [
+EXAMPLE1 = [20] + list(range(1, 20)) + list(range(21, 26))
+EXAMPLE2 = EXAMPLE1[1:] + [45]
+EXAMPLE3 = [
+    35,
+    20,
+    15,
+    25,
+    47,
+    40,
+    62,
+    55,
+    65,
+    95,
+    102,
+    117,
+    150,
+    182,
+    127,
+    219,
+    299,
+    277,
+    309,
+    576,
 ]
 
 
@@ -22,3 +45,54 @@ def test_part1(puzzle_input, answer):
 ])
 def test_part2(puzzle_input, answer):
     assert part2(puzzle_input) == answer
+
+
+@pytest.mark.parametrize('number, preamble, expected', [
+    (26, EXAMPLE1, True),
+    (49, EXAMPLE1, True),
+    (100, EXAMPLE1, False),
+    (50, EXAMPLE1, False),
+    (26, EXAMPLE2, True),
+    (65, EXAMPLE2, False),
+    (64, EXAMPLE2, True),
+    (66, EXAMPLE2, True),
+])
+def test_is_valid(number, preamble, expected):
+    assert is_valid(number, preamble) == expected
+
+
+def test_first_invalid():
+    assert first_invalid(EXAMPLE3, 5) == 127
+
+
+PREAMBLE = [1, 2, 3, 4]
+PREAMBLE_SUMS = {
+    1: {3, 4, 5},
+    2: {5, 6},
+    3: {7, },
+    4: set(),
+}
+UPDATED_SUMS = {
+    2: {5, 6, 7},
+    3: {7, 8},
+    4: {9, },
+    5: set(),
+}
+
+
+def test_all_sums():
+    assert all_sums(PREAMBLE) == PREAMBLE_SUMS
+
+
+def test_update_sums():
+    assert update_sums(PREAMBLE_SUMS, 5) == UPDATED_SUMS
+
+
+@pytest.mark.parametrize('number, sums, expected', [
+    (5, PREAMBLE_SUMS, True),
+    (10, PREAMBLE_SUMS, False),
+    (2, PREAMBLE_SUMS, False),
+    (8, PREAMBLE_SUMS, False),
+])
+def test_is_valid2(number, sums, expected):
+    assert is_valid2(number, sums) == expected
