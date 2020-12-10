95b2168eca16f1266ffcda7276672f43dc9a317f
Seth Guberman <sguberman@gmail.com>
2020-12-10 11:19:02 -0600

    start day10

---
+++ day10.py
@@ -0,0 +1,15 @@
+from utils import elapsed_time, print_results
+
+
+def part1(filename: str) -> int:
+    pass
+
+
+def part2(filename: str) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    puzzle_input = 'test_input_day10.txt'
+    print_results(elapsed_time(part1, puzzle_input),
+                  elapsed_time(part2, puzzle_input))
---
+++ test_day10.py
@@ -0,0 +1,70 @@
+import pytest
+
+from day10 import part1, part2
+
+PUZZLE_INPUT = 'input_day10.txt'
+TEST_INPUT = 'test_input_day10.txt'
+
+EXAMPLE1 = [
+    16,
+    10,
+    15,
+    5,
+    1,
+    11,
+    7,
+    19,
+    6,
+    12,
+    4,
+]
+
+EXAMPLE2 = [
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+]
+
+
+@pytest.mark.parametrize('puzzle_input, answer', [
+    (TEST_INPUT, 0),
+    (PUZZLE_INPUT, 0),
+])
+def test_part1(puzzle_input, answer):
+    assert part1(puzzle_input) == answer
+
+
+@pytest.mark.parametrize('puzzle_input, answer', [
+    (TEST_INPUT, 0),
+    (PUZZLE_INPUT, 0),
+])
+def test_part2(puzzle_input, answer):
+    assert part2(puzzle_input) == answer
