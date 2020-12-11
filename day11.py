554e7d9c6913db49fb49fdcbcccd80dc46370e44
Seth Guberman <sguberman@gmail.com>
2020-12-11 11:57:38 -0600

    solve day11 part 1

--- day11.py
+++ day11.py
@@ -1,8 +1,70 @@
+from typing import List, Tuple
+
 from utils import elapsed_time, print_results
+
+State = List[str]
+
+FLOOR = '.'
+EMPTY = 'L'
+OCCUPIED = '#'
+DIRECTIONS = [
+    (-1, -1), (-1,  0), (-1,  1),
+    (0,  -1),           (0,   1),
+    (1,  -1), (1,   0), (1,   1),
+]
+
+
+def read_seats(filename: str) -> State:
+    return open(filename).read().splitlines()
+
+
+def count_neighbors(i: int, j: int, state: State) -> int:
+    count = 0
+    for (ns, ew) in DIRECTIONS:
+        row, col = i + ns, j + ew
+        if (0 <= row < len(state)) and (0 <= col < len(state[0])):
+            neighbor = state[i + ns][j + ew]
+            if neighbor == OCCUPIED:
+                count += 1
+    return count
+
+
+def next_seat(seat: str, i: int, j: int, state: State) -> str:
+    if seat == FLOOR:
+        return FLOOR
+    elif (seat == EMPTY) and (count_neighbors(i, j, state) == 0):
+        return OCCUPIED
+    elif (seat == OCCUPIED) and (count_neighbors(i, j, state) >= 4):
+        return EMPTY
+    else:
+        return seat
+
+
+def simulate_one_round(state: State) -> Tuple[State, State]:
+    next_state: State = []
+    for i, row in enumerate(state):
+        next_row = ''
+        for j, seat in enumerate(row):
+            next_row += next_seat(seat, i, j, state)
+        next_state.append(next_row)
+    return state, next_state
+
+
+def simulate_to_end(state: State) -> State:
+    before, after = simulate_one_round(state)
+    while before != after:
+        before, after = simulate_one_round(after)
+    return after
+
+
+def count_occupied_seats(state: State) -> int:
+    return sum(sum(seat == OCCUPIED for seat in row) for row in state)
 
 
 def part1(filename: str) -> int:
-    pass
+    initial_state = read_seats(filename)
+    final_state = simulate_to_end(initial_state)
+    return count_occupied_seats(final_state)
 
 
 def part2(filename: str) -> int:
@@ -10,6 +72,6 @@ def part2(filename: str) -> int:
 
 
 if __name__ == '__main__':
-    puzzle_input = 'test_input_day11.txt'
-    print_results(elapsed_time(part1, puzzle_input),
+    puzzle_input = 'input_day11.txt'
+    print_results(elapsed_time(part1, puzzle_input),  # 2441
                   elapsed_time(part2, puzzle_input))
--- test_day11.py
+++ test_day11.py
@@ -1,24 +1,137 @@
 import pytest
 
-from day11 import part1, part2
+from day11 import (count_neighbors, count_occupied_seats, next_seat, part1,
+                   part2, simulate_one_round, simulate_to_end)
 
-PUZZLE_INPUT = 'input_dayXX.txt'
-TEST_INPUT = 'test_input_dayXX.txt'
+PUZZLE_INPUT = 'input_day11.txt'
+TEST_INPUT = 'test_input_day11.txt'
 EXAMPLE = [
+    'L.LL.LL.LL',
+    'LLLLLLL.LL',
+    'L.L.L..L..',
+    'LLLL.LL.LL',
+    'L.LL.LL.LL',
+    'L.LLLLL.LL',
+    '..L.L.....',
+    'LLLLLLLLLL',
+    'L.LLLLLL.L',
+    'L.LLLLL.LL',
+]
+ROUND1 = [
+    '#.##.##.##',
+    '#######.##',
+    '#.#.#..#..',
+    '####.##.##',
+    '#.##.##.##',
+    '#.#####.##',
+    '..#.#.....',
+    '##########',
+    '#.######.#',
+    '#.#####.##',
+]
+ROUND2 = [
+    '#.LL.L#.##',
+    '#LLLLLL.L#',
+    'L.L.L..L..',
+    '#LLL.LL.L#',
+    '#.LL.LL.LL',
+    '#.LLLL#.##',
+    '..L.L.....',
+    '#LLLLLLLL#',
+    '#.LLLLLL.L',
+    '#.#LLLL.##',
+]
+ROUND3 = [
+    '#.##.L#.##',
+    '#L###LL.L#',
+    'L.#.#..#..',
+    '#L##.##.L#',
+    '#.##.LL.LL',
+    '#.###L#.##',
+    '..#.#.....',
+    '#L######L#',
+    '#.LL###L.L',
+    '#.#L###.##',
+]
+ROUND4 = [
+    '#.#L.L#.##',
+    '#LLL#LL.L#',
+    'L.L.L..#..',
+    '#LLL.##.L#',
+    '#.LL.LL.LL',
+    '#.LL#L#.##',
+    '..L.L.....',
+    '#L#LLLL#L#',
+    '#.LLLLLL.L',
+    '#.#L#L#.##',
+]
+ROUND5 = [
+    '#.#L.L#.##',
+    '#LLL#LL.L#',
+    'L.#.L..#..',
+    '#L##.##.L#',
+    '#.#L.LL.LL',
+    '#.#L#L#.##',
+    '..L.L.....',
+    '#L#L##L#L#',
+    '#.LLLLLL.L',
+    '#.#L#L#.##',
 ]
 
 
 @pytest.mark.parametrize('puzzle_input, answer', [
-    (TEST_INPUT, 0),
-    (PUZZLE_INPUT, 0),
+    (TEST_INPUT, 37),
+    (PUZZLE_INPUT, 2441),
 ])
 def test_part1(puzzle_input, answer):
     assert part1(puzzle_input) == answer
 
 
 @pytest.mark.parametrize('puzzle_input, answer', [
-    (TEST_INPUT, 0),
-    (PUZZLE_INPUT, 0),
+    #(TEST_INPUT, 0),
+    #(PUZZLE_INPUT, 0),
 ])
 def test_part2(puzzle_input, answer):
     assert part2(puzzle_input) == answer
+
+
+def test_count_occupied_seats():
+    assert count_occupied_seats(ROUND5) == 37
+
+
+@pytest.mark.parametrize('state, next_state', [
+    (EXAMPLE, ROUND1),
+    (ROUND1, ROUND2),
+    (ROUND2, ROUND3),
+    (ROUND3, ROUND4),
+    (ROUND4, ROUND5),
+    (ROUND5, ROUND5),
+])
+def test_simulate_one_round(state, next_state):
+    before, after = simulate_one_round(state)
+    assert before == state
+    assert after == next_state
+
+
+@pytest.mark.parametrize('i, j, state, expected', [
+    (0, 0, EXAMPLE, 0),
+    (0, 0, ROUND1, 2),
+    (0, 0, ROUND2, 1),
+    (0, 0, ROUND3, 1),
+    (0, 0, ROUND4, 1),
+    (0, 0, ROUND5, 1),
+    (1, 4, EXAMPLE, 0),
+    (1, 4, ROUND1, 5),
+    (1, 4, ROUND2, 0),
+    (1, 4, ROUND3, 3),
+    (1, 4, ROUND4, 0),
+    (1, 4, ROUND5, 0),
+    (9, 9, EXAMPLE, 0),
+    (9, 9, ROUND1, 2),
+    (9, 9, ROUND2, 1),
+    (9, 9, ROUND3, 1),
+    (9, 9, ROUND4, 1),
+    (9, 9, ROUND5, 1),
+])
+def test_count_neighbors(i, j, state, expected):
+    assert count_neighbors(i, j, state) == expected
