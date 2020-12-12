0bdb3cfb82c7d6e43dedd0b401b21d23671c8533
Seth Guberman <sguberman@gmail.com>
2020-12-12 03:08:43 -0600

    solve day12 part 2

--- day12.py
+++ day12.py
@@ -30,8 +30,36 @@ def part1(filename: str) -> int:
     return abs(x) + abs(y)
 
 
+def rotate(dx, dy, angle):
+    if angle == 90 or angle == -270:
+        return -dy, dx
+    elif angle == 180 or angle == -180:
+        return -dx, -dy
+    elif angle == 270 or angle == -90:
+        return dy, -dx
+
+
 def part2(filename: str) -> int:
-    pass
+    x, y, dx, dy = 0, 0, 10, 1
+    instructions = (line.strip() for line in open(filename))
+    for instruction in instructions:
+        action, value = instruction[0], int(instruction[1:])
+        if action == 'N':
+            dy += value
+        elif action == 'S':
+            dy -= value
+        elif action == 'E':
+            dx += value
+        elif action == 'W':
+            dx -= value
+        elif action == 'L':
+            dx, dy = rotate(dx, dy, value)
+        elif action == 'R':
+            dx, dy = rotate(dx, dy, -value)
+        else:  # forward
+            x += value * dx
+            y += value * dy
+    return abs(x) + abs(y)
 
 
 if __name__ == '__main__':
