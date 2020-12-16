340ade9de9e62bc11902f98ad9ecbce4aec4ec24
Seth Guberman <sguberman@gmail.com>
2020-12-16 15:14:51 -0500

    solve day16 part 2

--- day16.py
+++ day16.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 from functools import partial
 from itertools import chain, filterfalse
-from typing import Dict, Iterable, List, TextIO, Tuple
+from math import prod
+from typing import Dict, Iterable, List, Optional, TextIO, Tuple
 
 from utils import elapsed_time, print_results
 
@@ -51,25 +53,92 @@ def read_nearby(file_handle: TextIO) -> List[Ticket]:
 
 
 def invalid_values(rules: Rules, tickets: List[Ticket]) -> Iterable[int]:
-    _is_valid = partial(is_valid, rules)
-    return filterfalse(_is_valid, chain.from_iterable(tickets))
+    _is_valid_value = partial(is_valid_value, rules)
+    return filterfalse(_is_valid_value, chain.from_iterable(tickets))
 
 
-def is_valid(rules: Rules, value: int) -> bool:
+def is_valid_value(rules: Rules, value: int) -> bool:
     all_ranges = chain.from_iterable(rules.values())
     return any(value in r for r in all_ranges)
+
+
+def valid_tickets(rules: Rules, tickets: List[Ticket]) -> List[Ticket]:
+    _is_valid_ticket = partial(is_valid_ticket, rules)
+    return list(filter(_is_valid_ticket, tickets))
+
+
+def is_valid_ticket(rules: Rules, ticket: Ticket) -> bool:
+    _is_valid_value = partial(is_valid_value, rules)
+    return all(_is_valid_value(value) for value in ticket)
+
+
+def ticket_columns(tickets: List[Ticket]) -> List[List[int]]:
+    return [[ticket[i] for ticket in tickets] for i in range(len(tickets[0]))]
+
+
+def match_fields(rules: Rules,
+                 tickets: List[Ticket]) -> Dict[str, List[int]]:
+    columns = ticket_columns(tickets)
+    matches = defaultdict(list)
+    for field in rules.keys():
+        for i, column in enumerate(columns):
+            if all(value in chain.from_iterable(rules[field]) for value in column):
+                matches[field].append(i)
+    return matches
+
+
+def eliminate_matches(matches: Dict[str, List[int]],
+                      todo: Optional[List[str]] = None) -> Dict[str, List[int]]:
+    if not todo:
+        todo = list(matches.keys())
+    else:
+        todo = todo.copy()
+
+    solved_fields = set()
+    while todo:
+        # Find a field that only matches one column
+        solved_field = next(field
+                            for field, matching_columns in matches.items()
+                            if field not in solved_fields
+                            and len(matching_columns) == 1)
+        if not solved_field:
+            break
+
+        # Remove that column from the other fields
+        matching_column = matches[solved_field][0]
+        for field in matches:
+            if field == solved_field:
+                continue
+            try:
+                matches[field].remove(matching_column)
+            except ValueError:
+                pass
+
+        # Track progress
+        solved_fields.add(solved_field)
+        try:
+            todo.remove(solved_field)
+        except ValueError:
+            pass
+
+    return matches
 
 
 def part1(rules: Rules, nearby: List[Ticket]) -> int:
     return sum(invalid_values(rules, nearby))
 
 
-def part2(filename: str) -> int:
-    pass
+def part2(rules: Rules, ticket: Ticket, nearby: List[Ticket]) -> int:
+    tickets = valid_tickets(rules, nearby)
+    todo = [field for field in rules if field.startswith('departure')]
+    matched_fields = eliminate_matches(match_fields(rules, tickets), todo)
+    return prod(ticket[columns[0]]
+                for field, columns in matched_fields.items()
+                if field in todo)
 
 
 if __name__ == '__main__':
     puzzle_input = 'input_day16.txt'
     rules, ticket, nearby = parse_input(puzzle_input)
     print_results(elapsed_time(part1, rules, nearby),  # 29759
-                  elapsed_time(part2, puzzle_input))
+                  elapsed_time(part2, rules, ticket, nearby))
--- test_day16.py
+++ test_day16.py
@@ -1,9 +1,11 @@
 import pytest
 
-from day16 import invalid_values, parse_input, part1, part2
+from day16 import (invalid_values, match_fields, parse_input, part1, part2,
+                   ticket_columns, valid_tickets)
 
 PUZZLE_INPUT = 'input_day16.txt'
-TEST_INPUT = 'test_input_day16.txt'
+TEST_INPUT_1 = 'test_input_day16_1.txt'
+TEST_INPUT_2 = 'test_input_day16_2.txt'
 
 EXAMPLE_RULES = {
     'class': [range(1, 4), range(5, 8)],
@@ -22,7 +24,7 @@ EXAMPLE_NEARBY = [
 
 
 @pytest.mark.parametrize('puzzle_input, answer', [
-    (TEST_INPUT, 71),
+    (TEST_INPUT_1, 71),
     (PUZZLE_INPUT, 29759),
 ])
 def test_part1(puzzle_input, answer):
@@ -31,17 +33,37 @@ def test_part1(puzzle_input, answer):
 
 
 @pytest.mark.parametrize('puzzle_input, answer', [
-    (TEST_INPUT, 0),
+    (TEST_INPUT_2, 0),
     (PUZZLE_INPUT, 0),
 ])
 def test_part2(puzzle_input, answer):
-    assert part2(puzzle_input) == answer
+    rules, ticket, nearby = parse_input(puzzle_input)
+    assert part2(rules, ticket, nearby) == answer
 
 
 def test_parse_input():
     expected = (EXAMPLE_RULES, EXAMPLE_TICKET, EXAMPLE_NEARBY)
-    assert parse_input(TEST_INPUT) == expected
+    assert parse_input(TEST_INPUT_1) == expected
 
 
 def test_invalid_values():
     assert list(invalid_values(EXAMPLE_RULES, EXAMPLE_NEARBY)) == [4, 55, 12]
+
+
+def test_valid_tickets():
+    assert valid_tickets(EXAMPLE_RULES, EXAMPLE_NEARBY) == [[7, 3, 47]]
+
+
+def test_ticket_columns():
+    assert ticket_columns(EXAMPLE_NEARBY) == [
+        [7, 40, 55, 38],
+        [3, 4, 2, 6],
+        [47, 50, 20, 12],
+    ]
+
+
+def test_match_fields():
+    rules, _, nearby = parse_input(TEST_INPUT_2)
+    tickets = valid_tickets(rules, nearby)
+    expected = {'class': [1], 'row': [0], 'seat': [2]}
+    assert match_fields(rules, tickets) == expected
