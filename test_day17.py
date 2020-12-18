3ec1c672dfe6a5526ae80b6e2e80f337cad9f481
Seth Guberman <sguberman@gmail.com>
2020-12-17 19:23:15 -0600

    fix tests for day17

--- test_day17.py
+++ test_day17.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 import pytest
 
 from day17 import (count_active_cubes, count_active_neighbors,
-                   expand_initial_grid, initialize_grid, neighboring_points,
-                   next_cube_state, part1, part2, print_grid, simulate_cycle)
+                   initialize_nd_grid, make_directions, neighboring_points,
+                   next_cube_state, part1, part2, simulate_cycle)
 
 PUZZLE_INPUT = 'input_day17.txt'
 TEST_INPUT = 'test_input_day17.txt'
@@ -18,17 +18,16 @@ def test_part1(puzzle_input, answer):
     assert part1(puzzle_input) == answer
 
 
-@pytest.mark.skip()
 @pytest.mark.parametrize('puzzle_input, answer', [
-    (TEST_INPUT, 0),
-    #(PUZZLE_INPUT, 0),
+    (TEST_INPUT, 848),
+    (PUZZLE_INPUT, 960),
 ])
 def test_part2(puzzle_input, answer):
     assert part2(puzzle_input) == answer
 
 
-def test_initialize_grid():
-    grid = initialize_grid(TEST_INPUT)
+def test_initialize_nd_grid():
+    grid, _ = initialize_nd_grid(TEST_INPUT)
     assert grid[(0, 0, 0)] == '.'
     assert grid[(1, 0, 0)] == '#'
     assert grid[(2, 0, 0)] == '.'
@@ -42,7 +41,8 @@ def test_initialize_grid():
 
 
 def test_neighboring_points():
-    neighbors = list(neighboring_points((0, 0, 0)))
+    directions = make_directions(3)
+    neighbors = list(neighboring_points(directions, (0, 0, 0)))
     assert len(neighbors) == 26
     assert (0, 0, 0) not in neighbors
     assert (1, 1, 1) in neighbors
@@ -60,58 +60,34 @@ def test_neighboring_points():
 
 
 def test_count_active_cubes():
-    grid = initialize_grid(TEST_INPUT)
+    grid, _ = initialize_nd_grid(TEST_INPUT)
     assert count_active_cubes(grid) == 5
 
 
 def test_simulate_cycle():
-    grid_0 = initialize_grid(TEST_INPUT)
-    grid_0 = expand_initial_grid(grid_0)
+    grid_0, directions = initialize_nd_grid(TEST_INPUT)
     #print("Before any cycles:\n")
     #print(print_grid(grid_0, 0))
-    grid_1 = simulate_cycle(grid_0)
+    grid_1 = simulate_cycle(grid_0, directions)
     #print("\nAfter 1 cycle:\n")
     #print(print_grid(grid_1, -1))
     #print(print_grid(grid_1, 0))
     #print(print_grid(grid_1, 1))
     assert count_active_cubes(grid_1) == 11
-    grid_2 = simulate_cycle(grid_1)
+    grid_2 = simulate_cycle(grid_1, directions)
     assert count_active_cubes(grid_2) == 21
-    grid_3 = simulate_cycle(grid_2)
+    grid_3 = simulate_cycle(grid_2, directions)
     assert count_active_cubes(grid_3) == 38
 
 
 def test_count_active_neighbors():
-    grid = initialize_grid(TEST_INPUT)
-    assert count_active_neighbors(grid, (0, 0, 0)) == 1
-    assert count_active_neighbors(grid, (1, 0, 0)) == 1
-    assert count_active_neighbors(grid, (2, 0, 0)) == 2
-    assert count_active_neighbors(grid, (0, 1, 0)) == 3
-    assert count_active_neighbors(grid, (1, 1, 0)) == 5
-    assert count_active_neighbors(grid, (2, 1, 0)) == 3
-    assert count_active_neighbors(grid, (0, 2, 0)) == 1
-    assert count_active_neighbors(grid, (1, 2, 0)) == 3
-    assert count_active_neighbors(grid, (2, 2, 0)) == 2
-
-
-EXAMPLE_1_n1 = initialize_grid('test_input_day17_1_n1.txt', -1)
-EXAMPLE_1_0 = initialize_grid('test_input_day17_1_0.txt', 0)
-EXAMPLE_1_1 = initialize_grid('test_input_day17_1_1.txt', 1)
-
-EXAMPLE_1 = EXAMPLE_1_n1 | EXAMPLE_1_0 | EXAMPLE_1_1
-
-
-def test_example():
-    for z in (-1, 0, 1):
-        print(print_grid(EXAMPLE_1, z))
-        print()
-    assert count_active_cubes(EXAMPLE_1) == 11
-
-    assert count_active_neighbors(EXAMPLE_1, (1, 1, 0)) == 10
-    assert next_cube_state(EXAMPLE_1, (1, 1, 0)) == '.'
-
-    assert count_active_neighbors(EXAMPLE_1, (0, 0, -1)) == 2
-    assert next_cube_state(EXAMPLE_1, (0, 0, -1)) == '#'
-
-    assert count_active_neighbors(EXAMPLE_1, (0, 2, -1)) == 3
-    assert next_cube_state(EXAMPLE_1, (0, 2, -1)) == '#'
+    grid, directions = initialize_nd_grid(TEST_INPUT)
+    assert count_active_neighbors(grid, directions, (0, 0, 0)) == 1
+    assert count_active_neighbors(grid, directions, (1, 0, 0)) == 1
+    assert count_active_neighbors(grid, directions, (2, 0, 0)) == 2
+    assert count_active_neighbors(grid, directions, (0, 1, 0)) == 3
+    assert count_active_neighbors(grid, directions, (1, 1, 0)) == 5
+    assert count_active_neighbors(grid, directions, (2, 1, 0)) == 3
+    assert count_active_neighbors(grid, directions, (0, 2, 0)) == 1
+    assert count_active_neighbors(grid, directions, (1, 2, 0)) == 3
+    assert count_active_neighbors(grid, directions, (2, 2, 0)) == 2
