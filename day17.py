847ab3f43476aa00ecb91c599005401d2ea4efba
Seth Guberman <sguberman@gmail.com>
2020-12-17 19:09:57 -0600

    refactor day17 for part 2

--- day17.py
+++ day17.py
@@ -1,58 +1,63 @@
 from collections import defaultdict
+from itertools import product
 from typing import Dict, Iterable, List, Tuple
 
 from utils import elapsed_time, print_results
 
-Point = Tuple[int, int, int]
+Point = Tuple[int, ...]
+Direction = Tuple[int, ...]
 Grid = Dict[Point, str]
 
 ACTIVE = '#'
 INACTIVE = '.'
-DIRECTIONS = [
-    (-1, -1,  1), (0, -1,  1), (1, -1,  1),
-    (-1,  0,  1), (0,  0,  1), (1,  0,  1),
-    (-1,  1,  1), (0,  1,  1), (1,  1,  1),
-
-    (-1, -1,  0), (0, -1,  0), (1, -1,  0),
-    (-1,  0,  0),              (1,  0,  0),
-    (-1,  1,  0), (0,  1,  0), (1,  1,  0),
-
-    (-1, -1, -1), (0, -1, -1), (1, -1, -1),
-    (-1,  0, -1), (0,  0, -1), (1,  0, -1),
-    (-1,  1, -1), (0,  1, -1), (1,  1, -1),
-]
 
 
-def initialize_grid(filename: str, at_z: int = 0) -> Grid:
+def initialize_nd_grid(filename: str,
+                       num_dimensions: int = 3) -> Tuple[Grid, List[Direction]]:
     grid = defaultdict(lambda: INACTIVE)
-    z = at_z
+    extra_dimensions = (num_dimensions - 2) * (0,)
     with open(filename) as f:
         for y, line in enumerate(f):
             for x, cube_state in enumerate(line.strip()):
-                grid[(x, y, z)] = cube_state
-    return grid
+                grid[(x, y) + extra_dimensions] = cube_state
+    directions = make_directions(num_dimensions)
+    expanded_grid = expand_initial_grid(grid, directions)
+    return expanded_grid, directions
 
 
-def neighboring_points(point: Point) -> Iterable[Point]:
-    for direction in DIRECTIONS:
-        yield tuple(sum(pair) for pair in zip(point, direction))
+def make_directions(num_dimensions: int = 3) -> List[Point]:
+    directions = list(product(*[(-1, 0, 1) for _ in range(num_dimensions)]))
+    directions.remove(num_dimensions * (0,))
+    return directions
 
 
-def count_active_neighbors(grid: Grid, point: Point) -> int:
-    # Grid will expand to include neighbors as they get checked
-    return sum(grid[point] == ACTIVE for point in neighboring_points(point))
-
-
-def expand_initial_grid(grid: Grid) -> Grid:
+def expand_initial_grid(grid: Grid, directions: List[Direction]) -> Grid:
     initial_points = list(grid.keys())
     for point in initial_points:
         # visit neighbors to expand the grid
-        neighbor_states = list(grid[n] for n in neighboring_points(point))
+        neighbor_states = list(grid[n]
+                               for n in neighboring_points(directions, point))
     return grid
 
 
-def next_cube_state(grid: Grid, point: Point) -> str:
-    active_neighbors = count_active_neighbors(grid, point)
+def neighboring_points(directions: List[Direction],
+                       point: Point) -> Iterable[Point]:
+    for direction in directions:
+        yield tuple(sum(pair) for pair in zip(point, direction))
+
+
+def count_active_neighbors(grid: Grid,
+                           directions: List[Direction],
+                           point: Point) -> int:
+    # Grid will expand to include neighbors as they get checked
+    return sum(grid[point] == ACTIVE
+               for point in neighboring_points(directions, point))
+
+
+def next_cube_state(grid: Grid,
+                    directions: List[Direction],
+                    point: Point) -> str:
+    active_neighbors = count_active_neighbors(grid, directions, point)
     cube_state = grid[point]
     if active_neighbors == 3:
         return ACTIVE
@@ -62,9 +67,11 @@ def next_cube_state(grid: Grid, point: Point) -> str:
         return INACTIVE
 
 
-def simulate_cycle(grid: Grid) -> Grid:
+def simulate_cycle(grid: Grid,
+                   directions: List[Direction]) -> Grid:
     points = list(grid.keys())
-    next_grid = {point: next_cube_state(grid, point) for point in points}
+    next_grid = {point: next_cube_state(grid, directions, point)
+                 for point in points}
     grid.update(next_grid)
     return grid
 
@@ -73,32 +80,24 @@ def count_active_cubes(grid: Grid) -> int:
     return sum(cube_state == ACTIVE for cube_state in grid.values())
 
 
-def print_grid(grid: Grid, at_z: int) -> str:
-    plane = [(x, y, z) for x, y, z in grid if z == at_z]
-    min_x = min(plane, key=lambda p: p[0])[0]
-    max_x = max(plane, key=lambda p: p[0])[0]
-    min_y = min(plane, key=lambda p: p[1])[1]
-    max_y = max(plane, key=lambda p: p[1])[1]
-    lines = [[grid[(x, y, at_z)] for x in range(min_x, max_x + 1)]
-             for y in range(min_y, max_y + 1)]
-    header = f'z={at_z}'
-    return '\n'.join([header] +
-                     [''.join(line) for line in lines])
+def simulate(filename: str, num_dimensions: int, num_cycles: int) -> Grid:
+    grid, directions = initialize_nd_grid(filename, num_dimensions)
+    for _ in range(num_cycles):
+        grid = simulate_cycle(grid, directions)
+    return grid
 
 
 def part1(filename: str) -> int:
-    grid = initialize_grid(filename)
-    grid = expand_initial_grid(grid)
-    for _ in range(6):
-        grid = simulate_cycle(grid)
+    grid = simulate(filename, 3, 6)
     return count_active_cubes(grid)
 
 
 def part2(filename: str) -> int:
-    pass
+    grid = simulate(filename, 4, 6)
+    return count_active_cubes(grid)
 
 
 if __name__ == '__main__':
-    puzzle_input = 'input_day17.txt'
+    puzzle_input = 'test_input_day17.txt'
     print_results(elapsed_time(part1, puzzle_input),  # 286
                   elapsed_time(part2, puzzle_input))
